bfbf49fe751e99de0ce648002e2204abc650145e
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-20 09:38:51 +0000

    reupload

---
+++ dotNotation.js
@@ -0,0 +1,26 @@
+/*
+console.log("My name is Jody".length); //logs length of string to the console
+
+console.log("heLLo World".toLowerCase()); //logs a string to the console after coverting characters to lower case
+
+console.log(Math.random()*10); //logs a random number to the console between 0 and 9.9999999
+
+console.log(Math.floor(Math.random()*500)); //logs a random number to the console between 0 and 499 (Math.floor removes decimals)
+
+
+console.log("      |       |      ");
+console.log("      |       |      ");
+console.log("      |       |      ");
+console.log("- - - - - - - - - - -");
+console.log("      |       |      ");
+console.log("      |       |      ");
+console.log("      |       |      ");
+console.log("- - - - - - - - - - -");
+console.log("      |       |      ");
+console.log("      |       |      ");
+console.log("      |       |      "); 
+*/
+
+
+console.log("      |       |      \n      |       |      \n      |       |      \n- - - - - - - - - - -\n      |       |      \n      |       |      \n      |       |      \n- - - - - - - - - - -\n      |       |      \n      |       |      \n      |       |      \n")
+
